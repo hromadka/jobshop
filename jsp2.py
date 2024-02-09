015d9d2568bebfb318d843eb9d20e7c824abad84
Ted Hromadka <hromadka@gmail.com>
2024-02-09 13:29:55 -0800

    define problem vars

--- jsp2.py
+++ jsp2.py
@@ -16,6 +16,31 @@ all_machines = range(machines_count)
 # Computes horizon dynamically as the sum of all durations.
 horizon = sum(task[1] for job in jobs_data for task in job)
 
+model = cp_model.CpModel()
 
+# Named tuple to store information about created variables.
+task_type = collections.namedtuple("task_type", "start end interval")
+# Named tuple to manipulate solution information.
+assigned_task_type = collections.namedtuple(
+    "assigned_task_type", "start job index duration"
+)
+
+# Creates job intervals and add to the corresponding machine lists.
+all_tasks = {}
+machine_to_intervals = collections.defaultdict(list)
+
+for job_id, job in enumerate(jobs_data):
+    for task_id, task in enumerate(job):
+        machine, duration = task
+        suffix = f"_{job_id}_{task_id}"
+        start_var = model.NewIntVar(0, horizon, "start" + suffix)
+        end_var = model.NewIntVar(0, horizon, "end" + suffix)
+        interval_var = model.NewIntervalVar(
+            start_var, duration, end_var, "interval" + suffix
+        )
+        all_tasks[job_id, task_id] = task_type(
+            start=start_var, end=end_var, interval=interval_var
+        )
+        machine_to_intervals[machine].append(interval_var)
 
 print("done")
