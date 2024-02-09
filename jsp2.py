9713365b1ca39425ad7d52634f59e94e330cb1f7
Ted Hromadka <hromadka@gmail.com>
2024-02-09 13:29:09 -0800

    define problem data

--- jsp2.py
+++ jsp2.py
@@ -4,3 +4,18 @@
 
 import collections
 from ortools.sat.python import cp_model
+
+jobs_data = [  # task = (machine_id, processing_time).
+    [(0, 3), (1, 2), (2, 2)],  # Job0
+    [(0, 2), (2, 1), (1, 4)],  # Job1
+    [(1, 4), (2, 3)],  # Job2
+]
+
+machines_count = 1 + max(task[0] for job in jobs_data for task in job)
+all_machines = range(machines_count)
+# Computes horizon dynamically as the sum of all durations.
+horizon = sum(task[1] for job in jobs_data for task in job)
+
+
+
+print("done")
